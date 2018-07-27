2c74800c23d7aa80aec5d925262b2de4d3a1d30b
leontaolong <longt8@uw.edu>
2018-07-26 17:24:16 -0700

    PLYT 1039 -- Inject a no-op link into the GraphQL client (#4)
    * Implement no-op deepDedupLink
    
    * Inject deepDedupLink into client
    
    * Add test cases for deepDedupLink
    
    * Lint fix
    
    * Organize code
    
    * Organize code
    
    * Add more tests for deepDedupLink
    
    * PLYT-1040--Add test cases that verify when cache access happens (#5)
    
    * Add cache monitoring tests in gql-client
    
    * Add cache access test in deepDedupLink
    
    * Fix import newline in /__tests__/client.ts
    
    * Refactor client so modules can be spied on in tests
    
    * Organize gql-client/package.json for the refactoring
    
    * Make CACHE_MOCK_DECLARATIONS as a variable
    
    * Zero index testIndex
    
    * PLYT-1041--Add test cases that verify when network requests are made (#6)
    
    * Add fetchMock and reportFetchStatus()
    
    * Print formatted query field in the report
    
    * Rebase on PLTY-1040
    
    * zero index testIndex
    
    * Add fetchMock and reportFetchStatus()
    
    * Print formatted query field in the report
    
    * Rebase on PLTY-1040
    
    * zero index testIndex
    
    * PLYT-1042--Add a test case demonstrating minimal query rewriting by a link (#7)
    
    * Add tests that verify query rewriting
    
    * Rewrite query name in deepDedupLink
    
    * Fix typo
    
    * Fix rebase conflict
    
    * Add helpful comments
    
    * Format imports
    
    * Assign query in each test for reporting query rewrite status in src/__tests__/client.ts
    
    * PLYT-1044--Add CI to GraphQL project (#8)
    
    * Add jenkinsfile and commands for CI
    
    * Updated Jenkinsfile
    
    * Cap Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update lerna run and test scripts for CI
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinfiles
    
    * Update Jenkinfiles
    
    * Update Jenkinfiles
    
    * Update Jenkinfiles
    
    * Update Jenkinfiles
    
    * Update Jenkinfiles
    
    * Update Jenkinfiles
    
    * Update Jenkinfiles
    
    * Update Jenkinfiles
    
    * Update Jenkinfiles
    
    * Update Jenkinfiles
    
    * Update Jenkinfiles
    
    * Update Jenkinfiles
    
    * Update Jenkinfiles
    
    * Update Jenkinfiles
    
    * Update Jenkinfiles
    
    * Update Jenkinfiles
    
    * Update Jenkinfiles
    
    * Update Jenkinfiles
    
    * Update Jenkinfiles
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Clean up and organize code
    
    * Update README
    
    * Add Run Linter stage
    
    * Cap Linter
    
    * Update README
    
    * Switch to a more universal FAILED_EMOJI and make them CONSTs
    
    * Fix slack message format

--- packages/apollo-link-deep-dedup/src/__tests__/deepDedupLink.ts
+++ packages/apollo-link-deep-dedup/src/__tests__/deepDedupLink.ts
@@ -1,7 +1,111 @@
 import 'jest';
 
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import {
+    ApolloLink,
+    DocumentNode,
+    execute,
+    GraphQLRequest,
+    Observable,
+} from 'apollo-link';
+import gql from 'graphql-tag';
+
+import { DeepDedupLink } from '../deepDedupLink';
+
 describe('DeepDedupLink', () => {
-    it(`runs tests successfully`, () => {
-        expect('hello jest').toBe('hello jest');
+    // shared dummy vars
+    const document: DocumentNode = gql`
+        query test1($x: String) {
+        test(x: $x)
+        }`;
+
+    const variables = { x: 'Hello World' };
+    const request: GraphQLRequest = {
+        query: document,
+        variables: variables,
+    };
+    const data = { test: 1 };
+
+    it(`runs query without errors`, async () => {
+        const link = ApolloLink.from([
+            new DeepDedupLink({ cache: new InMemoryCache() }),
+            new ApolloLink(() => {
+                return new Observable(observer => {
+                    observer.next({ data: data });
+                    observer.complete();
+                });
+            }),
+        ]);
+        execute(link, request).subscribe(result =>
+            expect(result).toBeTruthy(),
+        );
+    });
+
+    it(`bypasses deduplication as desired`, async () => {
+        const newRequest = Object.assign({}, request);
+        // add forceFetch rule
+        newRequest.context = {
+            forceFetch: true,
+        };
+        const link = ApolloLink.from([
+            new DeepDedupLink({ cache: new InMemoryCache() }),
+            new ApolloLink((operation) => {
+                // assert that query has not been modified
+                expect(operation.variables).toBe(request.variables);
+                expect(operation.query).toBe(request.query);
+                return new Observable(observer => {
+                    observer.complete();
+                });
+            }),
+        ]);
+        execute(link, newRequest);
+    });
+
+    it(`unsubscribes as needed`, () => {
+        let unsubscribed = false;
+        const link = ApolloLink.from([
+            new DeepDedupLink({ cache: new InMemoryCache() }),
+            new ApolloLink(() => {
+                return new Observable(() => {
+                    // triggered when unsubscribe() gets called
+                    return () => {
+                        unsubscribed = true;
+                    };
+                });
+            }),
+        ]);
+        const sub = execute(link, request).subscribe({});
+        // unsubscribe
+        sub.unsubscribe();
+        // assert
+        expect(unsubscribed).toBe(true);
+    });
+
+    it(`accesses cache`, () => {
+        const cache = new InMemoryCache();
+        // list of cache access functions,
+        // which will be specifically determined based on actual implementation later
+        const mockFuncNames = ['read', 'diff', 'readQuery', 'readFragment', 'extract'];
+        const cacheMocks: jest.SpyInstance<any>[] = mockFuncNames.map(funcName =>
+            jest.spyOn(cache, funcName as any),
+        );
+        const link = ApolloLink.from([
+            new DeepDedupLink({ cache: cache }),
+            new ApolloLink(() => {
+                let toHaveBeenCalled = false;
+                // inspect mocks in the following link
+                cacheMocks.forEach(mockFunc => {
+                    if (mockFunc.mock.calls.length > 0) {
+                        toHaveBeenCalled = true;
+                    }
+                });
+                // assert
+                expect(toHaveBeenCalled).toBe(true);
+                return new Observable(observer => {
+                    observer.complete();
+                });
+            }),
+        ]);
+        execute(link, request);
     });
 });
--- packages/gql-client/src/apolloClient.ts
+++ packages/gql-client/src/apolloClient.ts
@@ -1,17 +1,39 @@
-import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloCache } from 'apollo-cache';
 import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from "apollo-link-http";
-import fetch from 'cross-fetch';
+import { ApolloLink } from 'apollo-link';
+import { createHttpLink } from 'apollo-link-http';
+
+import { DeepDedupLink } from '../../apollo-link-deep-dedup/lib/index';
 
 const SERVER_URI = 'http://localhost:3000';
 const DATA_API_URI = `${SERVER_URI}/graphql`;
 
-const link = createHttpLink({
-    uri: DATA_API_URI,
-    fetch: fetch,
-});
+type FetchFunction = (
+    input?: string | Request | undefined,
+    init?: RequestInit | undefined,
+) => Promise<Response>;
 
-export const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    link, // [apollo-http-link]
-});
+const createClient = (cache: ApolloCache<any>, fetch: FetchFunction) => {
+    // Initialize Apollo Links
+    const deepDedupLink = new DeepDedupLink({
+        cache: cache,
+    });
+
+    const httpLink = createHttpLink({
+        uri: DATA_API_URI,
+        fetch: fetch,
+    });
+
+    const link = ApolloLink.from([
+        deepDedupLink,
+        httpLink,
+    ]);
+
+    // Initialize Apollo Client
+    return new ApolloClient({
+        cache: cache,
+        link,
+    });
+};
+
+export default createClient;
