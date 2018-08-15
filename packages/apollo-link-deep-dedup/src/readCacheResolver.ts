2fd1bd8f01c07ead40071aaa7b326f7b2bdcb325
leontaolong <longt8@uw.edu>
2018-08-15 16:55:30 -0700

    Implement readCacheResolver

--- packages/apollo-link-deep-dedup/src/readCacheResolver.ts
+++ packages/apollo-link-deep-dedup/src/readCacheResolver.ts
@@ -10,7 +10,6 @@ import {
 import {
     getStoreKeyName,
     isJsonValue,
-    toIdValue,
 } from 'apollo-utilities';
 
 /**
@@ -18,7 +17,7 @@ import {
  * @param {string} fieldName the name of the field to be resolved
  * @param {CacheDataIdObj} idValue an object holding dataId to the corresponding object in the cache
  * @param {any} args argument associated with this field
- * @param resolutionContext a context object holding field resolution related info
+ * @param {any} resolutionContext a context object holding field resolution related info
  * @returns {any} resolved data or undefined if missing
  */
 export const readCacheResolver: Resolver = (
@@ -27,11 +26,7 @@ export const readCacheResolver: Resolver = (
     args: any,
     resolutionContext: any,
 ) => {
-    const {
-        store, // cache data store
-        cacheRedirects,
-        dataIdFromObject,
-    } = resolutionContext;
+    const { store } = resolutionContext;  // cache data store
 
     // get the object from cache
     const objId: string = idValue.id;
@@ -42,32 +37,6 @@ export const readCacheResolver: Resolver = (
         // get field from object
         const storeKeyName: string = args ? getStoreKeyName(fieldName, args) : fieldName;
         fieldValue = obj[storeKeyName]; // can also be undefined if cache miss
-
-        // handle cache redirects if it's configured:
-        // https://www.apollographql.com/docs/react/advanced/caching.html#cacheRedirect
-        if (
-            typeof fieldValue === 'undefined' &&
-            cacheRedirects &&
-            (obj.__typename || objId === 'ROOT_QUERY')
-        ) {
-            const typename = obj.__typename || 'Query';
-            // Look for the type in the custom resolver map
-            const type = cacheRedirects[typename];
-            if (type) {
-                // Look for the field in the custom resolver map
-                const resolver = type[fieldName];
-                if (resolver) {
-                    fieldValue = resolver(obj, args, {
-                        getCacheKey(storeObj: StoreObject) {
-                            return toIdValue({
-                                id: dataIdFromObject(storeObj),
-                                typename: storeObj.__typename,
-                            });
-                        },
-                    });
-                }
-            }
-        }
 
         // if this is an object scalar, it must be a json blob and we have to unescape it
         // this is specific to the cache data in apollo-cache-inmemory
